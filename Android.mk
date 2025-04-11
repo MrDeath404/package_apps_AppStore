c0920735a3d5e304388bfaa6d0463fc8f4a50eb5
MrDeath404 <pisarekpatryk404@gmail.com>
2025-04-11 12:01:29 +0200

    Fixed

--- Android.mk
+++ Android.mk
@@ -1,14 +1,4 @@
-# Implementation of GrapheneOS AppStore for GMSCompat (for feature update)
-
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := AppStore
-LOCAL_SRC_FILE := prebuild/AppStore.apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_CLASS := $(TARGET_OUT)/app
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_PREBUILT)
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/prebuild/AppStore.apk:$(TARGET_OUT_DATA)/app/AppStore/AppStore.apk
